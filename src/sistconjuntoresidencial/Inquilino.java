3625202eb993f6284ac6d2d686b516773eac74fa
YinetRojasCampos20B <rojascamposyinet1101@gmail.com>
2026-09-16 17:36:45 -0500

    Añadiendo un método constructor para Ãla clase hija Inquilino, de la clase abstracta Residente

--- src/sistconjuntoresidencial/Inquilino.java
+++ src/sistconjuntoresidencial/Inquilino.java
@@ -6,6 +6,13 @@ public class Inquilino extends Residente {
 
     private boolean contratoVigente;
 
+    // Constructor de la clase Cliente
+
+    public Inquilino(String documento, String nombre, boolean activo, String torre, String apartamento, boolean contratoVigente) {
+        super(documento, nombre, activo, torre, apartamento);
+        this.contratoVigente = contratoVigente;
+    }
+
     // Getters y setter de la clase concreta Inquilino
 
     public boolean getContratoVigente() {
